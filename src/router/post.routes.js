a7823255ef38222a6c0faa6a981b04308e9e1e74
Jose Rdz <joseluisrdzs@live.com>
2022-05-14 02:44:21 -0500

    post endpoints

---
+++ src/router/post.routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const PostController = require('../controllers/post.controller');
+const PostMiddleware = require('../middlewares/post.middleware');
+
+router.get('/post', PostMiddleware.getPost, PostController.getPost);
+router.post('/post', PostMiddleware.createPost, PostController.createPost);
+
+
+module.exports = router;
