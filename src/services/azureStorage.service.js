2fea9065b248a730425b3fee9a7e98c38d2689f2
Jose Rdz <joseluisrdzs@live.com>
2022-05-14 15:29:15 -0500

    use uuid

---
+++ src/services/azureStorage.service.js
@@ -0,0 +1,24 @@
+const { v4 } = require('uuid');
+const getStream = require('into-stream');
+const { BlockBlobClient } = require('@azure/storage-blob');
+
+
+
+const { storageCStr, containerName } = require('../config/vars').azure;
+
+class AzureStorageService {
+  uploadFile = async (buffer, mimetype) => {
+    try {
+      const blobName = `${v4()}.${mimetype.split('/')[1]}`;
+      const blobService = new BlockBlobClient(storageCStr, containerName, blobName);
+      const stream = getStream(buffer);
+      const streamLength = buffer.length;
+
+      await blobService.uploadStream(stream, streamLength);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
+
+module.exports = new AzureStorageService();
